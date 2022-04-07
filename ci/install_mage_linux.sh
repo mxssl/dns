d238d519e61be0cb136bfd4e458c1064f09109c3
mxssl <m.susula@gmail.com>
2022-04-08 02:32:15 +0300

    update go version

--- ci/install_mage_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-wget https://github.com/magefile/mage/releases/download/v1.9.0/mage_1.9.0_Linux-64bit.tar.gz
-tar zvxf mage_1.9.0_Linux-64bit.tar.gz
-cp mage /usr/local/bin/mage
-chmod +x /usr/local/bin/mage
